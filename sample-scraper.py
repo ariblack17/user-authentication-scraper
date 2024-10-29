10de93693a15cdc4a199596208c2bce1989cfb52
ari <ariannahblack@gmail.com>
2024-10-29 15:15:14 -0400

    add more sample code

--- sample-scraper.py
+++ sample-scraper.py
@@ -1,15 +1,34 @@
 
 from selenium import webdriver                  ## to control the browser
 from selenium.webdriver.common.keys import Keys ## to simulate key presses
+from selenium.webdriver.common.by import By     ## to locate elements within a document
 
 
 
-## create a web driver instance (must be in the same directory as the script)
+## create a web driver instance (only few browsers are supported)
 driver = webdriver.Chrome()     ## opens a Chrome browser window
 
 ## load a website
 driver.get("https://www.python.org")    ## waits for the page to load completely
 
 ## check page title
-print(driver.title) 
+print(f'page title: {driver.title}') 
+assert "Python" in driver.title
+
+## interact with the search bar
+search_bar = driver.find_element(By.NAME, 'q')  ## finds search bar element
+search_bar.clear()                              ## clears any existing text
+search_bar.send_keys('pycon')                   ## types query into the search bar
+search_bar.send_keys(Keys.RETURN)               ## simulates pressing return key
+
+## check the updated url
+print(f'new url: {driver.current_url}')
+assert "No results found." not in driver.page_source    ## check that some results were found
+
+## close the tab
+driver.close()
+
+## close the window
+# driver.quit()
+
 
